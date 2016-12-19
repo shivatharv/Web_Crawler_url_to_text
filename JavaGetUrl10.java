ea1993317a942699cebef5b2cbfd638399ca9c50
shivatharv <shivanisinghal@iiitdmj.ac.in>
2016-12-19 13:20:11 -0500

    Add files via upload

---
+++ JavaGetUrl10.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.*;
+
+public class JavaGetUrl10{
+	public static void generator() {
+
+		URL url;
+		try {
+			File files = new File("C:/Users/shivani/workspace/crawler/src/Employee.txt");
+			FileReader fileReader = new FileReader(files);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			//StringBuffer stringBuffer = new StringBuffer();
+			String line;
+			int i=1;
+			while ((line = bufferedReader.readLine()) != null) {
+						try {
+							// get URL content
+							url = new URL(line);
+							URLConnection conn = url.openConnection();
+
+							// open the stream and put it into BufferedReader
+							BufferedReader br = new BufferedReader(
+											   new InputStreamReader(conn.getInputStream()));
+
+							String inputLine;
+
+							//save to this filename
+							String fileName = "C:/Users/shivani/workspace/crawler/src/test"+i+".html";
+							File file = new File(fileName);
+
+							if (!file.exists()) {
+								file.createNewFile();
+							}
+
+							//use FileWriter to write file
+							FileWriter fw = new FileWriter(file.getAbsoluteFile());
+							BufferedWriter bw = new BufferedWriter(fw);
+
+							while ((inputLine = br.readLine()) != null) {
+								bw.write(inputLine);
+							}
+
+							bw.close();
+							br.close();
+
+							System.out.println("Done");
+
+						} catch (MalformedURLException e) {
+							e.printStackTrace();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						i++;
+			}
+			fileReader.close();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+}
